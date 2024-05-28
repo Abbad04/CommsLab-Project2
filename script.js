6f473714ec63df528e3aa1666a89f350b8ad027e
Abbad04 <79320108+Abbad04@users.noreply.github.com>
2024-05-29 01:13:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', function(){
     const image = document.querySelector('#cat');
 
     image.addEventListener('mouseover', function(){
-        this.src = 'orangeCat2.svg';
+        this.src = 'media/orangeCat2.svg';
     });
 
     image.addEventListener('mouseout', function(){
-        this.src = 'orangeCat.svg';
+        this.src = 'media/orangeCat.svg';
     });
     
 })
